8fed695d605068f66c95a3200c7146e5ec2ba8fe
Jiffy <jifffffy@gmail.com>
2021-06-25 11:29:17 +0800

    temp

--- plugins/xfs/src/main/java/io/jiffy/stax/plugin/xfs/actions/service/CardIsrCrdAction.java
+++ plugins/xfs/src/main/java/io/jiffy/stax/plugin/xfs/actions/service/CardIsrCrdAction.java
@@ -5,7 +5,7 @@ import io.jiffy.stax.plugin.xfs.actions.XfsServiceAction;
 public class CardIsrCrdAction extends XfsServiceAction {
 
     public CardIsrCrdAction() {
-        super("cardIsr");
+        super("cardIsrCrd");
     }
 
 }
